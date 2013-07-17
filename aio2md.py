930769c7cec1b58e056ec9599a9f4aa2774c30bf
haya14busa <hayabusa1419@gmail.com>
2013-07-18 02:34:53 +0900

    del files

--- aio2md.py
+++ aio2md.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     finally:
         aiofile.close()
     # text = 'He grinned and said, "I make lots of money. On weekdays I receive an average of 50 orders a day from all over the globe via the Internet."'
-    trtxt = re.sub(r'\b([a-zA-z]+)\b', r'[\1]', aio_all_text)
+    trtxt = re.sub(r'\b([a-zA-z]+)\b', r'[\1] ', aio_all_text) # 2 space needed
 
     aio_file_object = open('aio_etm_txt.md', 'w')
     aio_file_object.write(trtxt)
